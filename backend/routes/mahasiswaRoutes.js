d225614899ca094947b05e4cb5078b5fbba1f3b1
graceseliandareva <graceseliandareva@gmail.com>
2026-06-25 23:13:02 +0700

    fix: replace uploadToSupabase with uploadToCloudinary, add upload middleware to PUT logbook

--- backend/routes/mahasiswaRoutes.js
+++ backend/routes/mahasiswaRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken, authorizeRoles } = require('../middleware/authMiddleware');
-const { upload, uploadToSupabase } = require('../middleware/uploadMiddleware');
+const { upload, uploadToCloudinary } = require('../middleware/uploadMiddleware');
 const {
   getPengajuan, tambahPengajuan, updatePengajuan, hapusPengajuan,
   getLogbook, tambahLogbook, updateLogbook, hapusLogbook,
-  getDokumen, uploadDokumen,
+  getDokumen, uploadDokumen, hapusDokumen, resubmitDokumen,
   getFeedback, getPenilaian, getNotifikasi, getPeriodeAktif
 } = require('../controllers/mahasiswaController');
 
@@ -25,22 +25,24 @@ router.get('/logbook', auth, getLogbook);
 router.post('/logbook', auth, (req, res, next) => {
   req.uploadFolder = 'logbook-bukti';
   next();
-}, upload.single('bukti'), uploadToSupabase, tambahLogbook);
-router.put('/logbook/:id', auth, updateLogbook);
+}, upload.single('bukti'), uploadToCloudinary, tambahLogbook);
+router.put('/logbook/:id', auth, (req, res, next) => {
+  req.uploadFolder = 'logbook-bukti';
+  next();
+}, upload.single('bukti'), uploadToCloudinary, updateLogbook);
 router.delete('/logbook/:id', auth, hapusLogbook);
 
 // Dokumen
 router.get('/dokumen', auth, getDokumen);
 router.post('/dokumen', auth, (req, res, next) => {
-  req.uploadFolder = 'temp';
+  req.uploadFolder = 'dokumen-pendukung';
   next();
-}, upload.single('file'), uploadToSupabase, uploadDokumen);
-router.delete('/dokumen/:id', auth, require('../controllers/mahasiswaController').hapusDokumen);
-
+}, upload.single('file'), uploadToCloudinary, uploadDokumen);
+router.delete('/dokumen/:id', auth, hapusDokumen);
 router.put('/dokumen/:id/resubmit', auth, (req, res, next) => {
-  req.uploadFolder = 'temp';
+  req.uploadFolder = 'dokumen-pendukung';
   next();
-}, upload.single('file'), uploadToSupabase, require('../controllers/mahasiswaController').resubmitDokumen);
+}, upload.single('file'), uploadToCloudinary, resubmitDokumen);
 
 // Feedback & penilaian
 router.get('/feedback', auth, getFeedback);
